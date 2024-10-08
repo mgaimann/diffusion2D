c5baefbd422f08f9b640667c99200cc83eb71a0a
mgaimann <gaimannscience@gmail.com>
2024-10-08 11:56:07 +0200

    handing over dt, T_cold, T_hot to plotting

--- diffusion2d.py
+++ diffusion2d.py
@@ -8,40 +8,6 @@ import numpy as np
 
 from output import output_plots
 
-# plate size, mm
-w = h = 10.
-# intervals in x-, y- directions, mm
-dx = dy = 0.1
-# Thermal diffusivity of steel, mm^2/s
-D = 4.
-
-# Initial cold temperature of square domain
-T_cold = 300
-
-# Initial hot temperature of circular disc at the center
-T_hot = 700
-
-# Number of discrete mesh points in X and Y directions
-nx, ny = int(w / dx), int(h / dy)
-
-# Computing a stable time step
-dx2, dy2 = dx * dx, dy * dy
-dt = dx2 * dy2 / (2 * D * (dx2 + dy2))
-
-print("dt = {}".format(dt))
-
-u0 = T_cold * np.ones((nx, ny))
-u = u0.copy()
-
-# Initial conditions - circle of radius r centred at (cx,cy) (mm)
-r, cx, cy = 2, 5, 5
-r2 = r ** 2
-for i in range(nx):
-    for j in range(ny):
-        p2 = (i * dx - cx) ** 2 + (j * dy - cy) ** 2
-        if p2 < r2:
-            u0[i, j] = T_hot
-
 
 def do_timestep(u_nm1, u, D, dt, dx2, dy2):
     # Propagate with forward-difference in time, central-difference in space
@@ -53,17 +19,47 @@ def do_timestep(u_nm1, u, D, dt, dx2, dy2):
     return u_nm1, u
 
 
-def solve(u0, u, D, dt, dx2, dy2, nsteps=101):
+def solve(dx=0.1, dy=0.1, D=4., nsteps=101):
+    # plate size, mm
+    w = h = 10.
+
+    # Initial cold temperature of square domain
+    T_cold = 300
+
+    # Initial hot temperature of circular disc at the center
+    T_hot = 700
+
+    # Number of discrete mesh points in X and Y directions
+    nx, ny = int(w / dx), int(h / dy)
+
+    # Computing a stable time step
+    dx2, dy2 = dx * dx, dy * dy
+    dt = dx2 * dy2 / (2 * D * (dx2 + dy2))
+
+    print("dt = {}".format(dt))
+
+    u0 = T_cold * np.ones((nx, ny))
+    u = u0.copy()
+
+    # Initial conditions - circle of radius r centred at (cx,cy) (mm)
+    r, cx, cy = 2, 5, 5
+    r2 = r ** 2
+    for i in range(nx):
+        for j in range(ny):
+            p2 = (i * dx - cx) ** 2 + (j * dy - cy) ** 2
+            if p2 < r2:
+                u0[i, j] = T_hot
+
     # Time loop
     us = []
     for n in range(nsteps):
         u0, u = do_timestep(u0, u, D, dt, dx2, dy2)
         us.append(u.copy())
 
-    return us
+    return us, dt, T_cold, T_hot
 
 
 n_output = [0, 10, 50, 100]
-us = solve(u0, u, D, dt, dx2, dy2, nsteps=101)
+us, dt, T_cold, T_hot = solve()
 fig = output_plots(us, n_output, dt, T_cold, T_hot)
 fig.show()
